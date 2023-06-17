e543921a680f85cdb980eef89c7f98e33626f7cc
tsunufu <tsunufu@icloud.com>
2023-06-17 18:39:36 +0900

    complete Task tasks

--- main.go
+++ main.go
@@ -11,9 +11,12 @@ import (
 func main() {
 	db := db.NewDB()
 	userRepository := repository.NewUserRepository(db)
+	taskRepository := repository.NewTaskRepository(db)
 	userUsecase := usecase.NewUserUsecase(userRepository)
+	taskUsecase := usecase.NewTaskUsecase(taskRepository)
 	userController := controller.NewUserController(userUsecase)
-	e := router.NewRouter(userController)
+	taskController := controller.NewTaskController(taskUsecase)
+	e := router.NewRouter(userController, taskController)
 	e.Logger.Fatal(e.Start(":8080"))
 	// godotenv.Load()
 }
